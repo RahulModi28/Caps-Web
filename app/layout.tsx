ae18bf3542c17490c54b3876cd83a53344451734
Sung-Jinwoo-28 <rmodi182@gmail.com>
2026-06-08 10:37:48 +0530

    chore: install Google Tag Manager (GTM)

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PND8WWWX');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta charSet="utf-8" />
         <link href="/css/caps-shared.css" rel="stylesheet" type="text/css" />
         <link href="/css/caps-layout.css" rel="stylesheet" type="text/css" />
@@ -77,6 +88,16 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PND8WWWX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
 
         {/* Local jQuery, GSAP and Interaction scripts */}
